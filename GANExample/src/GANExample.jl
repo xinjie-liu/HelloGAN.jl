d22acc3d1051f30972e83f879858bcb266860872
xinjie-liu <xinjieliu98@163.com>
2024-04-19 14:21:34 -0500

    a working GAN gradient computation loop

--- GANExample/src/GANExample.jl
+++ GANExample/src/GANExample.jl
@@ -2,9 +2,12 @@ module GANExample
 
 using Flux: Flux, gradient, Optimise.update!, params, glorot_uniform, Dense, Optimise.Adam, NNlib.relu, NNlib.gelu, NNlib.elu, NNlib.sigmoid,
 Chain, @functor, train!, cpu, gpu, softplus, params, BatchNorm, normalise
+using Zygote: Zygote
+using Optimisers: Optimisers, setup, update, destructure
 using Random: Random
 using Distributions: Distributions, MixtureModel, Normal, MvNormal
 using Makie: Makie, Axis
+using GLMakie
 using CUDA
 
 include("gan.jl")
--- GANExample/src/gan.jl
+++ GANExample/src/gan.jl
@@ -4,17 +4,57 @@ struct GAN
     discriminator::Any
 end
 
-function get_generator_loss(gan::GAN; rng, device)
-    function loss(x)
-        
+function get_generator_loss(generator, discriminator)
+    function loss(ϵ)
+        generated_fake_data = generator(ϵ)
+        sum(log.(1 .- discriminator(generated_fake_data) .+ 1e-6)) / size(ϵ)[2] # according to Goodfellow et al., use log(G(ϵ)) instead for improved gradient signal
     end
 end
 
-function train_gan(; set_up = construct_training_setup())
+function get_discriminator_loss(discriminator)
+    function loss(real_data, fake_data)
+        -(sum(log.(discriminator(real_data) .+ 1e-6)) + sum(log.(1 .- discriminator(fake_data) .+ 1e-6))) / size(real_data)[2]
+    end
+end
+
+function train_gan(; set_up = construct_training_setup(), training_log_sample_size = 1000)
     gan = setup_gan(set_up)
+    fixed_ϵ = rand(set_up.rng, Distributions.Normal(), gan.z_dim, training_log_sample_size) # fixed noise samples for computing loss
+    fixed_data = set_up.dataset[:, 1:training_log_sample_size]
     for epoch in 1:set_up.training_config.n_epochs
         println("Epoch $epoch")
-        
+        ii = 0
+        for mini_batch in set_up.data_batch_iterator
+            num_samples = size(mini_batch)[2]
+            if ii % (set_up.training_config.time_difference_k + 1) != 0
+                # update discriminator
+                ϵ = rand(set_up.rng, Distributions.Normal(), gan.z_dim, num_samples) # noise samples
+                fake_data = gan.generator(ϵ)
+                # explicit style of gradient computation
+                flat_model, reconstruct_ = destructure(gan.discriminator)
+                grads = Zygote.gradient(flat_model) do flat_
+                    reconstructed_model = reconstruct_(flat_)
+                    loss = get_discriminator_loss(reconstructed_model)
+                    loss(mini_batch, fake_data)
+                end
+                grads[1]
+            else
+                # update generator
+                ϵ = rand(set_up.rng, Distributions.Normal(), gan.z_dim, num_samples) # noise samples
+                # explicit style of gradient computation
+                flat_model, reconstruct_ = destructure(gan.generator)
+                grads = Zygote.gradient(flat_model) do flat_
+                    reconstructed_model = reconstruct_(flat_)
+                    loss = get_generator_loss(reconstructed_model, gan.discriminator)
+                    loss(ϵ)
+                end
+                grads[1]
+            end
+            ii += 1
+        end
+        current_loss = (sum(log.(gan.discriminator(fixed_data) .+ 1e-6)) 
+        + sum(log.(1 .- gan.discriminator(gan.generator(fixed_ϵ)) .+ 1e-6))) / training_log_sample_size
+        @info "loss: $(current_loss)"
     end
 end
 
@@ -30,7 +70,8 @@ function construct_training_setup()
         n_epochs = 200,
         batchsize = 128,
         n_datapoints = 100_000,
-        device = gpu
+        device = gpu,
+        time_difference_k = 3, # difference of the update frequency between the generator and the discriminator
     )
 
     dims = (; dim_x = 1, dim_hidden = 32, dim_z = 1) # dim_x: data dimension dim_z: 
